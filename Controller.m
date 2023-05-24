111c02b69453de57c187b6e1ced828f157ea784e
Qinghua Ma <3392550566@qq.com>
2023-05-24 13:20:21 +0800

    Create-20230524

---
+++ Controller.m
@@ -0,0 +1,66 @@
+classdef Controller < handle
+    properties
+        viewObj;
+        modelObj;
+    end
+
+    methods
+        % Constructor of 'Controller' class 
+        function obj = Controller(viewObj,modelObj)
+            obj.viewObj = viewObj;
+            obj.modelObj = modelObj;
+        end
+        
+        function controller_connectDevice(obj,~,~)
+            obj.modelObj.callback_connectDevice();
+        end
+
+        function controller_disconnectDevice(obj,~,~)
+            obj.modelObj.callback_disconnectDevice();
+        end
+
+        function controller_runCollecting(obj,~,~)
+            obj.modelObj.callback_captureData();
+        end
+
+        function controller_updateChannelSettings(obj,~,~)
+            obj.modelObj.callback_changeChannelSettings;
+        end
+
+        function controller_updateChannelRangeSettings(obj,~,~)
+            obj.modelObj.callback_changeChannelRange;
+        end
+
+        function controller_closeApp(obj,~,~)
+            obj.modelObj.callback_closeApp();
+        end
+
+        function controller_saveData(obj,~,~)
+            obj.modelObj.callback_saveData();
+        end
+
+        function controller_clearAxes(obj,~,~)
+            obj.modelObj.callback_clearAxes();
+        end
+
+        function controller_updateEnableCheckBox(obj,~,~)
+            obj.modelObj.callback_updateEnableCheckBox();
+        end
+
+        function controller_stopCaptureData(obj,src,~)
+            obj.modelObj.callback_stopCaptureData(src);
+        end
+
+        function controller_updateTriggerSettings(obj,~,~)
+            obj.modelObj.callback_updateTriggerSettings();
+        end
+
+        function controller_updateAutoTriggerEnable(obj,~,~)
+            obj.modelObj.callback_updateAutoTriggerEnable();
+        end
+
+        function controller_loadMatDataFile(obj,~,~)
+            obj.modelObj.callback_loadMatDataFile();
+        end
+    end
+end
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# MATLAB-based-picoscope-4824A-data-acquisition-system
+# MATLAB Based PicoScope 4824A Block Data Acquisition System
+
+This is a simple MATLAB-based PicoScope 4824A Data Acquisition System, preliminarily achieving block data acquisition, display and storage.
+
+![image-20230524125228712](https://blogimages-1309804558.cos.ap-nanjing.myqcloud.com/DeLLLaptop/image-20230524125228712.png)
+
+<br>
+
+# Requirements
+
+- Hardware: [PicoScope 4824A Oscilloscope (PicoScope 4000A Series)](https://www.picotech.com/oscilloscope/4000/picoscope-4000-series) by [Pico Technology Ltd](https://www.picotech.com/).
+- Required MATLAB Toolboxes
+  - MATLAB Instrument Control Toolbox: [Instrument Control Toolbox - MATLAB](https://ww2.mathworks.cn/en/products/instrument.html).
+  - MATLAB PicoScope Support Toolbox: [picotech/picosdk-matlab-picoscope-support-toolbox: A Toolbox for use with Pico Technology Instrument Drivers. - GitHub](https://github.com/picotech/picosdk-matlab-picoscope-support-toolbox).
+  - MATLAB PicoScope 4000 Series A API MATLAB Generic Instrument Driver: [picotech/picosdk-ps4000a-matlab-instrument-driver: MATLAB® Instrument Driver for use with PicoScope® 4000 (A API) Series oscilloscopes - GitHub](https://github.com/picotech/picosdk-ps4000a-matlab-instrument-driver).
+
+Additionally, icons of GUI Buttons are available at: [Remix-Design/RemixIcon: Open source neutral style icon system - GitHub](https://github.com/Remix-Design/RemixIcon).
+
+<br>
+
+# Usage
+
+Download and unzip this repository, and then run the `run.m`.
